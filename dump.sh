f7b62c07bc2347531619ea7878372ff4a43137e9
Juhyung Park <qkrwngud825@gmail.com>
2023-09-12 21:58:11 +0900

    Allow subsequent downloads to be done in parallel
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- dump.sh
+++ dump.sh
@@ -4,29 +4,37 @@ set -ex
 # Extract full update
 aria2c -x5 $1 -o ota.zip
 unzip ota.zip payload.bin
+mv payload.bin payload_working.bin
 TAG="`unzip -p ota.zip META-INF/com/android/metadata | grep ^version_name= | cut -b 14-`"
 BODY="[$TAG]($1) (full)"
 rm ota.zip
 mkdir ota
-./bin/ota_extractor -output_dir ota -payload payload.bin
-rm payload.bin
+(
+    ./bin/ota_extractor -output_dir ota -payload payload_working.bin
+    rm payload_working.bin
+) & # Allow subsequent downloads to be done in parallel
 
 # Apply incrementals
 for i in ${@:2}; do
     aria2c -x5 $i -o ota.zip
     unzip ota.zip payload.bin
+    wait
+    mv payload.bin payload_working.bin
     TAG="`unzip -p ota.zip META-INF/com/android/metadata | grep ^version_name= | cut -b 14-`"
     BODY="$BODY -> [$TAG]($i)"
     rm ota.zip
 
-    mkdir ota_new
-    ./bin/ota_extractor -input-dir ota -output_dir ota_new -payload payload.bin
+    (
+        mkdir ota_new
+        ./bin/ota_extractor -input-dir ota -output_dir ota_new -payload payload_working.bin
 
-    rm -rf ota
-    mv ota_new ota
+        rm -rf ota
+        mv ota_new ota
 
-    rm payload.bin
+        rm payload_working.bin
+    ) & # Allow subsequent downloads to be done in parallel
 done
+wait
 
 # Compress with zstd
 zstd --rm ota/*
