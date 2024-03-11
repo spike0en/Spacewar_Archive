5718d788bac680dc55823677274986697e006657
spike0en <spike0en@gmail.com>
2024-03-11 10:21:12 +0530

    Revamp script and new instructions
    - Organize the stock OTA images into .7z archives with categorized boot, firmware, and logical partitions. (Ref commit: 0ea9029d38037ea4bfa2480c31bb113e23d6bcef)
    - Implement Base Full OTA uploading in a split 7z format. (Ref commit: 3dd27736278d54bb05e7cbc8720fd6cc9a19380b)
    - Add support for gdrive links & provide hash for base ota file. (Ref commit: 2f556cb1ca78f07c038432bef1cebd0a24fdb353)

--- README.md
+++ README.md
@@ -1,16 +1,34 @@
 # Nothing Phone (1) Archive
 
-* Comprehensive Collection of Unmodified Stock OTA Images for Nothing Phone (1) 
-* A personally maintained fork of [Nothing Archive by arter97](https://github.com/arter97/nothing_archive)
+* A comprehensive collection of unmodified Full OTA update packages & stock OTA images for Nothing Phone (1).
 
 
 ## Downloads
 
 - Downloads are tagged with POST_OTA_VERSION & NothingOS version [here](https://github.com/spike0en/Spacewar_Archive/releases). Please note that certain early releases for `T1.5` may lack `POST_OTA_VERSION` information. In such instances, downloads are provided with custom names, such as `Spacewar_T1.5_XXXXXX-XXXX_NothingOS Version`.
 
-- Logical partition images are uploaded separately, marked -logical,
-  so that other images can be downloaded quickly if needed.
+- Official Full OTA Update Package is marked `-FullOTA`. Extract the splitted 7z parts to get the `<name>-FullOTA.zip`.
 
+- The Stock OTA image files are categorized and archived in .7z format based on `boot`, `logical`, and `firmware` partitions:
+
+### Boot Partition (marked `-image-boot.7z`)
+
+Includes 4 images:
+```bash
+boot, vendor_boot, recovery & vbmeta
+```
+### Logical partition (marked `-image-logical.7z.001-003`)
+
+Includes 7 images:
+```bash
+system, system_ext, product, vendor, odm, vbmeta_system & vbmeta_vendor
+```
+### Firmware partition (marked `-image-firmware.7z`)
+
+Includes 24 images:
+```bash
+abl, aop, aop_config, bluetooth, cpucp, devcfg, dsp, dtbo, featenabler, hyp, imagefv, keymaster, modem, multiimgoem, multiimgqti, qupfw, qweslicstore, shrm, tz, uefi, uefisecapp, xbl, xbl_config & xbl_ramdump
+```
 
 ## Disclaimer
 
@@ -18,8 +36,9 @@
 
 - You can re-lock the bootloader after flashing images.
 
-- For further inquiries, discussions, and engaging content, users are encouraged to explore the [Nothing Phone (1) Telegram Community](https://t.me/NothingPhone1)
+- SHA-1 hash of `<name>-FullOTA.zip` file has been provided. It is to be noted that the built-in NothingOS Offline Updater Tool autonomously verifies file integrity. It initiates the update process only if the file aligns with the hash values specified in `payload-properties.txt`, which is obtained during the creation of the update package.
 
+- For further inquiries, discussions, and engaging content, users are encouraged to explore the [Nothing Phone (1) Telegram Community](https://t.me/NothingPhone1)
   
 ## Fastboot Flashing
 
@@ -33,6 +52,22 @@
 ``` bash
 fastboot update --disable-verity --disable-verification vbmeta.img
 ```
+
+## Manual Sideloading of Full OTA Packages
+
+### A. Via Stock Nothing Offline OTA Updater Tool (Locked BL): 
+
+1. To flash stock, unmodified official Full OTA packages, extract the files using 7z to obtain the <name>-FullOTA.zip file.
+2. Using your preferred file manager, create a folder named 'ota' at the root of your storage.
+3. Copy the <name>-FullOTA.zip into the newly created 'ota' folder.
+4. Open your dial pad and type `*#*#682#*#*`.
+5. The manual update utility will launch, scanning and locating your previously downloaded update file.
+6. Tap to begin the update. The process will take about 10-15 minutes (duration may vary).
+7. Enjoy your updated device after reboot!
+
+### B. Via Custom Recovery (Unlocked BL):
+
+- Alternatively, users can directly flash these full OTA packages using available custom recoveries for Nothing Phone (1).
 
 
 ## Integrity Check
@@ -50,5 +85,5 @@ xxh128sum -c *-hash.xxh128
 
 
 ### Thanks to
-- [luk1337](https://github.com/luk1337/oplus_archive)
-- [arter97](https://github.com/arter97/nothing_archive)
+- [luk1337](https://github.com/luk1337/oplus_archive) & [arter97](https://github.com/arter97/nothing_archive) for their great work!
+- [LukeSkyD](https://github.com/LukeSkyD) for ota link captures.
--- dump.sh
+++ dump.sh
@@ -1,61 +1,120 @@
 #!/bin/bash
-set -ex
+# Script has been modified by Spike for additional functionality and clarity, originally by arter97 and luk1337.
 
-# Extract full update
-aria2c -x5 $1 -o ota.zip
+# Set execute permissions for ota_extractor
+chmod +x ./bin/ota_extractor
+
+# Download base ota package with aria2c or gdown based on link format while renaming it to ota.zip
+download_with_gdown() {
+    gdown --fuzzy "$url" -O ota.zip
+}
+
+download_with_aria2c() {
+    aria2c -x5 "$1" -o ota.zip
+}
+
+download_file() {
+    local url="$1"
+
+    if [[ "$url" == *"drive.google.com"* ]]; then
+        download_with_gdown "$url"
+    else
+        download_with_aria2c "$url"
+    fi
+}
+
+# Call download_file function with the provided URL
+download_file "$1"
+
+# Create a new folder 'fullota' and copy ota.zip to it
+mkdir fullota
+cp ota.zip fullota
+
+# Continue with unzipping and extracting the payload
 unzip ota.zip payload.bin
 mv payload.bin payload_working.bin
 TAG="`unzip -p ota.zip payload_properties.txt | grep ^POST_OTA_VERSION= | cut -b 18-`"
 BODY="[$TAG]($1) (full)"
 rm ota.zip
 mkdir ota
-(
-    ./bin/ota_extractor -output_dir ota -payload payload_working.bin
-    rm payload_working.bin
-) & # Allow subsequent downloads to be done in parallel
+
+# Perform extraction on payload_working.bin
+./bin/ota_extractor -output_dir ota -payload payload_working.bin
 
 # Apply incrementals
-for i in ${@:2}; do
-    aria2c -x5 $i -o ota.zip
+for i in "${@:2}"; do
+    download_file "$i"
     unzip ota.zip payload.bin
-    wait
     mv payload.bin payload_working.bin
     TAG="`unzip -p ota.zip payload_properties.txt | grep ^POST_OTA_VERSION= | cut -b 18-`"
     BODY="$BODY -> [$TAG]($i)"
     rm ota.zip
 
-    (
-        mkdir ota_new
-        ./bin/ota_extractor -input-dir ota -output_dir ota_new -payload payload_working.bin
+    mkdir ota_new
+    ./bin/ota_extractor -input-dir ota -output_dir ota_new -payload payload_working.bin
 
-        rm -rf ota
-        mv ota_new ota
+    rm -rf ota
+    mv ota_new ota
 
-        rm payload_working.bin
-    ) & # Allow subsequent downloads to be done in parallel
+    rm payload_working.bin
 done
-wait
 
-# Create a split 7z image
-mkdir out
-mkdir dyn
+# Create necessary folders to be used later (`dyn` for logical images and `syn` for boot partition images)
+mkdir out dyn syn
+
+# Switch back to `ota` directory
 cd ota
-# Calculate hash
-for h in md5 sha1 sha256; do
-  ls * | parallel openssl dgst -$h -r | sort -k2 -V > ../out/${TAG}-hash.$h &
+
+# Calculate the hashes for all files in the `ota` directory and send them to `out` (tagged with `-hash`)
+for h in md5 sha1 sha256 xxh128; do
+    if [ "$h" = "xxh128" ]; then
+        ls * | parallel xxh128sum | sort -k2 -V > ../out/${TAG}-hash.$h
+    else
+        ls * | parallel "openssl dgst -${h} -r" | sort -k2 -V > ../out/${TAG}-hash.${h}
+    fi
 done
-ls * | parallel xxh128sum | sort -k2 -V > ../out/${TAG}-hash.xxh128 &
-wait
-for f in system system_ext product vendor odm; do
+
+# Move specific boot partition image files from `ota` to `syn` directory
+for f in boot recovery vendor_boot vbmeta; do
+    mv ${f}.img ../syn
+done
+
+# Switch to `ota` directory and move specific logical partition image files from `ota` to `dyn` directory
+cd ../ota
+for f in system system_ext product vendor odm vbmeta_system vbmeta_vendor; do
     mv ${f}.img ../dyn
 done
-7z a -mx6 ../out/${TAG}-image.7z * &
-cd ../dyn
-7z a -mx6 -v1g ../out/${TAG}-image-logical.7z *
-wait
-cd ..
-rm -rf ota dyn
 
-# Echo tag name and release body
+# Switch to `syn` directory and create a 7z archive for boot partition images tagged with "-boot"
+cd ../syn
+7z a -mmt4 -mx6 ../out/${TAG}-image-boot.7z *
+rm -rf ../syn
+
+# Switch to `ota` directory and create a 7z archive for firmware images tagged with "-firmware"
+cd ../ota
+7z a -mmt4 -mx6 ../out/${TAG}-image-firmware.7z *
+rm -rf ../ota
+
+# Switch to `dyn` directory and create a split 7z archive for logical images tagged with "-logical"
+cd ../dyn
+7z a -mmt4 -mx6 -v1g ../out/${TAG}-image-logical.7z *
+wait
+rm -rf ../dyn
+
+# Switch to `fullota` directory
+cd ../fullota
+
+# Copy ota.zip and rename it to ${TAG}-FullOTA.zip
+cp ota.zip "./${TAG}-FullOTA.zip"
+
+# Calculate SHA-1 hash for the FullOTA file and send them to `out` (tagged with `-FullOTA-hash`)
+SHA1_HASH=$(openssl dgst -sha1 -r "${TAG}-FullOTA.zip" | cut -d ' ' -f 1)
+echo "${SHA1_HASH}" > "../out/${TAG}-FullOTA-hash.sha1"
+
+# Create a split 7z archive for the base FullOTA Package
+7z a -mmt4 -mx6 -v1g "../out/${TAG}-FullOTA.7z" "${TAG}-FullOTA.zip"
+rm -rf ../fullota
+
+# Echo tag name, release body, and release history
 echo "tag=$TAG" >> "$GITHUB_OUTPUT"
 echo "body=$BODY" >> "$GITHUB_OUTPUT"
